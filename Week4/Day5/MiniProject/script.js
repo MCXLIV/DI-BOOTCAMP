896a7a59cfebd621f82ca473b6bb6c75805df94b
mcxliv <markdcole83@gmail.com>
2024-06-23 16:13:10 +0300

    update

--- Week4/Day5/MiniProject/script.js
+++ Week4/Day5/MiniProject/script.js
@@ -5,26 +5,24 @@ function playTheGame(){
         return
     }
     const computerNumber = generateRandomNumber()
-    console.log(computerNumber)
+    const compareNumbers = computerNumber   
+    console.log(computerNumber) 
     let answer = prompt ('Enter a number')
     let numberofTries = 0
     while( Number(answer) != compareNumbers){
         numberofTries = numberofTries + 1
         if(numberofTries === 3){
-            alert('Sorry you lost all your tries, the correct number was', computerNumber)
+            alert('Sorry you lost all your tries')
             return
         }       
-
         if(!isOnlyNumbers(answer)){
             alert('Sorry not a number, Goodbye ')
                 return
             }
-
             if (!isBetweenZeroandTen( Number(answer))){
                 alert('Sorry that was not a good number, goodbye')
                 return
-            }
-            console.log( Number(answer), computerNumber)
+            }   
             if( Number(answer) > computerNumber ){
                 alert("Your number is larger than the computer's, guess again")
                 
@@ -34,9 +32,9 @@ function playTheGame(){
                
             }
             answer = prompt ('Enter a number')
-    }
+    }  if (Number(answer) === computerNumber){
   alert('Congrats, you found the number', computerNumber)
-
+    }
 }
     function isOnlyNumbers(str){
         const regex = new RegExp(/^[0-9]*$/)
@@ -49,7 +47,7 @@ function playTheGame(){
     return Math.floor(Math.random()*11)
     }
 
-    function compareNumbers(userNumber, computerNumber){
+function compareNumbers(userNumber, compareNumbers){
         if(userNumber === compareNumbers){
             alert ('Winner')
             return
